0e662aa6bc195e21e1ffda36782b34cc1e8f1ae6
Devin100086 <116065959+Devin100086@users.noreply.github.com>
2024-01-12 00:55:06 +0800

    fix: fix some bug

--- src/store/index.js
+++ src/store/index.js
@@ -44,7 +44,7 @@ export default createStore({
       "MANAGER":"经理",
       "SUPER_ADMIN":"系统管理员",
       "STORAGE":"库房",
-      "Cashier":"收银",
+      "CASHIER":"收银",
       "CUSTOMER_SERVICE":"导购",
       "VICE_MANAGER":"副经理",
       "GROUP_MANAGER":"小组长"
